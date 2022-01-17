67e3679e59bb23dc50495f1bfd499f4a57cf2b71
Rauf <79477491+rauf95@users.noreply.github.com>
2022-01-17 13:21:58 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"strconv"
 )
+
+var db *sql.DB
+
+type UpdateReq struct {
+	Type   string `json:"type"`
+	Region string `json:"region"`
+}
 
 type Forest struct {
 	ID     int    `json:"id"`
@@ -32,24 +41,37 @@ func main() {
 	router.HandleFunc("/forest/{id}", updateForest).Methods("PUT")
 	router.HandleFunc("/forest/{id}", deleteForest).Methods("DELETE")
 
+	connStr := "user=postgres password=postgres sslmode=disable"
+	var err error
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Printf("Start server 2000\n")
 	log.Fatal(http.ListenAndServe(":2000", router))
 }
+
 
 func getForest(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	params, _ := strconv.Atoi(mux.Vars(r)["id"])
 
-	for _, item := range forests {
-		if item.ID == params {
-			err := json.NewEncoder(w).Encode(item)
-			if err != nil {
-				return
-			}
-		}
+	f := Forest{}
+	err := db.QueryRowContext(r.Context(), `SELECT * FROM forests WHERE id = $1`, params).Scan(&f.ID, &f.Type, &f.Region)
+	if err != nil {
+		panic(err)
 	}
+	json.NewEncoder(w).Encode(f)
 }
+
 
 func getAllForest(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -60,45 +82,75 @@ func getAllForest(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+
 func createForest(w http.ResponseWriter, r *http.Request) {
 	var newForest Forest
 	json.NewDecoder(r.Body).Decode(&newForest)
-	forests = append(forests, newForest)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(forests)
 
-}
-
-func updateForest(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params, _ := strconv.Atoi(mux.Vars(r)["id"])
-
-	for index, item := range forests {
-		if item.ID == params {
-			forests = append(forests[:index], forests[index+1])
-			var forest Forest
-			_ = json.NewDecoder(r.Body).Decode(&forest)
-			forest.ID = params
-			forests = append(forests, forest)
-			err := json.NewEncoder(w).Encode(forest)
-			if err != nil {
-				return
-			}
-		}
+	err := db.QueryRowContext(r.Context(), "insert into forests (region, kind) values ($1,$2) returning id", newForest.Region, newForest.Type).
+		Scan(&newForest.ID)
+	if err != nil {
+		log.Println("ERROR", "msg", err)
+		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newForest)
+}
+
+func updateForest(w http.ResponseWriter, r *http.Request) {
+
+	idParams := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idParams)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("ID couldn't be converted to integer"))
+		return
+	}
+
+	updateReq := UpdateReq{}
+	err = json.NewDecoder(r.Body).Decode(&updateReq)
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("couldn't unmarshal request"))
+		return
+	}
+
+	for i := range forests {
+
+		if id == forests[i].ID {
+			forests[i].Type = updateReq.Type
+			forests[i].Region = updateReq.Region
+		}
+	}
+	_, err = db.ExecContext(r.Context(), "update forests set region = $1, kind = $2 where id = $3   ", updateReq.Region, updateReq.Type, id)
+	if err != nil {
+		log.Println("ERROR", "msg", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 }
 
 func deleteForest(w http.ResponseWriter, r *http.Request) {
+	params, err := strconv.Atoi(mux.Vars(r)["id"])
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("couldn't parse id"))
+		return
+	}
+
+	_, err = db.ExecContext(r.Context(), "delete from forests where id = $1", params)
+	if err != nil {
+		log.Println("ERROR", "msg", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
 	w.Header().Set("Content-Type", "application/json")
-	params, _ := strconv.Atoi(mux.Vars(r)["id"])
-	for i, item := range forests {
-		if item.ID == params {
-			forests = append(forests[:i], forests[i+i])
-			fmt.Printf("forest with ID %v is deleted", params)
-
-		}
-	}
+	w.WriteHeader(http.StatusOK)
 }
